81f3b190d6721f731db3e3f04b5507372992749e
Aidan Cooney <acooney613@gmail.com>
2023-11-26 13:17:06 -0700

    update

--- plots.py
+++ plots.py
@@ -1,1 +1,9 @@
 import streamlit as st
+import seaborn as sns
+import pandas as pd
+
+df = pd.read_html('https://www.baseball-reference.com/awards/hof.shtml')[0]
+
+user_input = st.text_input('Enter a Year', 2023)
+
+st.write(user_input)
