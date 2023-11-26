9e2c3ccf97081180ec66dfaf3a32ea6add2a2aa4
Aidan Cooney <acooney613@gmail.com>
2023-11-26 13:49:22 -0700

    update

--- plots.py
+++ plots.py
@@ -1,12 +1,15 @@
 import streamlit as st
-#import pandas as pd
+import pandas as pd
 #import plotly.express as px
 
-st.title('Popular US Names')
+#st.title('Popular US Names')
 
 
-#df = pd.read_html('https://www.baseball-reference.com/awards/hof.shtml')
+df = pd.read_html('https://www.baseball-reference.com/awards/hof.shtml')
+df = df[0]
+#print(df)
+st.dataframe(df)
 
-user_input = st.text_input('Enter a Year', 2023)
+#user_input = st.text_input('Enter a Year', 2023)
 
-st.write(user_input)
+#st.write(user_input)
