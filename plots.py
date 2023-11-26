fcf9206e173a1723fec09bd8e994c8f9c55e0953
Aidan Cooney <acooney613@gmail.com>
2023-11-26 13:54:45 -0700

    update

--- plots.py
+++ plots.py
@@ -4,14 +4,11 @@ import pandas as pd
 
 #st.title('Popular US Names')
 
-url = 'https://www.baseball-reference.com/awards/hof.shtml'
-dfs = pd.read_html(url)
+df = pd.DataFrame({'column1' : [1, 2, 3, 4, 5], 'column2' : [1, 2, 3, 4, 5]})
+
+st.dataframe(df)
 
 # Display each dataframe using st.write()
-for i, df in enumerate(dfs):
-    st.write(f"DataFrame {i+1}")
-    st.dataframe(df)  # You can also use st.table(df) if you prefer a table view
-    st.write("---") 
 
 #user_input = st.text_input('Enter a Year', 2023)
 
